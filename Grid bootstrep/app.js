fe434ad83ba5e83a4ed9636d47d306265e0d3dbe
yan resende <saresende555@gmail.com>
2021-06-17 21:05:05 -0300

    tudo certo

---
+++ Grid bootstrep/app.js
@@ -0,0 +1,46 @@
+const TMDB_ENDPOINT = 'https://api.themoviedb.org/3';
+const APIKEY = '9c2c75d9d67c3f98cc98bb29450115c5';
+const IMG_PREFIX = 'https://image.tmdb.org/t/p/w500';
+let xhr;
+
+function carregaFilmes () {
+    xhr = new XMLHttpRequest ();
+
+    xhr.open ('GET', TMDB_ENDPOINT + '/movie/popular' + '?api_key=' + APIKEY, true);
+    xhr.onload = exibeFilmes;
+    xhr.send();
+}
+
+function pesquisaFilmes () {
+    xhr = new XMLHttpRequest ();
+
+    query = document.getElementById('pesquisa').value;
+
+    xhr.open ('GET', TMDB_ENDPOINT + '/search/movie' + '?api_key=' + APIKEY + '&query=' + query, true);
+    xhr.onload = exibeFilmes;
+    xhr.send();
+}
+
+
+function exibeFilmes () {
+    
+    let data = JSON.parse (xhr.responseText);
+    let textoHTML = '';
+
+    for (let i = 0; i < data.results.length; i++) {
+        let nomeFilme = data.results[i].title;
+        let sinopse = data.results[i].overview;
+        let imagem = IMG_PREFIX + data.results[i].poster_path;
+
+        textoHTML += `<div class="card col-md-4">
+            <img src="${imagem}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${nomeFilme}</h5>
+                <p class="card-text">${sinopse}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>`
+    }
+
+    document.getElementById('tela').innerHTML = textoHTML;
+}
